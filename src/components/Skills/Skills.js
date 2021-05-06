6e55bace9cfb16a2187218f8a47c20b121fe2025
Luke Sparg <lukesparg@hotmail.ca>
2021-05-05 20:31:13 -0700

    Filled the skills sections with content. Updated the experience section to use the same format as the skills section.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,8 +3,8 @@ import { Row, Col, Container, Button, ButtonGroup } from "reactstrap";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { ReactComponent as ArrowLeft } from "../../assets/arrow-left.svg"
 import { ReactComponent as ArrowRight } from "../../assets/arrow-right.svg"
-
 import { constrainLoop } from "../../function/MathFunction";
+import { useSpring, animated } from 'react-spring'
 
 import './style.css';
 
@@ -12,33 +12,104 @@ export const Skills = ({ darkModeClass }) => {
 
     const [activeSkill, setActiveSkill] = useState("data");
     const skillInfo = {
-        "data": "Data Info",
-        "web": "Web Info",
-        "extra": "Extra Info",
+        "data": {
+            "title": "Data Science and Analytics",
+            "text": "I have strong knowledge of statistical principles for applications in data collection, manipulation and interpretation. Using these skills with machine learning I have created models for both prediction and inference. Some of these skills include:",
+            "list": [
+                "Python for data scripting inluding libraries like pandas, numpy, and sklearn.",
+                "SQL for data storage and retrieval. I am also open to learn other relational and non-relational languages/systems.",
+                "NLP techniques including tokenization, pre-training, finetuning and evaluation metrics.",
+                "Understanding of bias, error, regression, trees, as well as other metrics and models."
+            ]
+        },
+        "web": {
+            "title": "Web and Application Developement", 
+            "text": "I have interned and taken course work in web developement to help bridge the online gap between companies and clients. An example of this work is the webpage you are currently viewing. Some technologies I have worked with include:",
+            "list": [
+                "Javascript for frontend developement, using pure JS as well as frameworks like React.",
+                "Javascript and Python backends to manage secure transfer of information",
+                "PHP sites. Including Wordpress knowledge.",
+                "Using HTML and CSS with design principles to create stunning UIs."
+            ]
+        },
+        "extra": {
+            "title": "Additional Skills",
+            "text": "I have worked with various other technologies and concepts to complement my other skills, including:",
+            "list": [
+                "Git / Github / Bitbucket",
+                "Strong understanding of Java and object oriented programming",
+                "Software Developement Lifecycles",
+                "Client and Customer Relations",
+                "Container technology using Docker (Interested in equivalent technologies such as Kubernetes as well).",
+                "Familiar using offsight servers to run jobs and applications."
+            ]
+        },
     };
+
+    //Slow pulse animation to be used by multiple divs in render/return
+    const pulseAnimation = useSpring({
+        from: {
+            opacity: 1
+        },
+        to: async (next, cancel) => {
+            let toggle = 1;
+            while (true) {
+                    await next({
+                        opacity: toggle
+                    });
+                    toggle = (toggle === 1) ? 0.3 : 1;
+            }
+        }
+    });
 
 
     return (
         <Row>
             <Col sm="4">
                 <Row>
-                    <div onClick={() => setActiveSkill("data")} className={"skill-title-circle" + ((activeSkill === "data") ? "-active" : "") + darkModeClass}>
-                        DATA
-                    </div>
+                    {(activeSkill !== "data") ?
+                        <animated.div style={ pulseAnimation } onClick={() => setActiveSkill("data")} className={"skill-title-box" + darkModeClass}>
+                            DATA
+                        </animated.div>
+                        :
+                        <div onClick={() => setActiveSkill("data")} className={"skill-title-box-active" + darkModeClass}>
+                            DATA
+                        </div>
+                    }
                 </Row>
                 <Row>
-                    <div onClick={() => setActiveSkill("web")} className={"skill-title-circle" + ((activeSkill === "web") ? "-active" : "") + darkModeClass}>
-                        WEB
-                    </div>
+                    {(activeSkill !== "web") ?
+                        <animated.div style={ pulseAnimation } onClick={() => setActiveSkill("web")} className={"skill-title-box" + darkModeClass}>
+                            WEB
+                        </animated.div>
+                        :
+                        <div onClick={() => setActiveSkill("web")} className={"skill-title-box-active" + darkModeClass}>
+                            WEB
+                        </div>
+                    }
                 </Row>
                 <Row>
-                    <div onClick={() => setActiveSkill("extra")} className={"skill-title-circle" + ((activeSkill === "extra") ? "-active" : "") + darkModeClass}>
-                        EXTRA
-                    </div>
+                    {(activeSkill !== "extra") ?
+                        <animated.div style={ pulseAnimation } onClick={() => setActiveSkill("extra")} className={"skill-title-box" + darkModeClass}>
+                            EXTRA
+                        </animated.div>
+                        :
+                        <div onClick={() => setActiveSkill("extra")} className={"skill-title-box-active" + darkModeClass}>
+                            EXTRA
+                        </div>
+                    }
                 </Row>
             </Col>
             <Col sm="8">
-                {skillInfo[activeSkill]}
+                <h2>{skillInfo[activeSkill]["title"]}</h2>
+                <p>{skillInfo[activeSkill]["text"]}</p>
+                <ul>
+                    {skillInfo[activeSkill]["list"].map((item, index) =>
+                        <li key={index}>
+                            {item}
+                        </li>
+                    )}
+                </ul>
             </Col>
         </Row>
     );
